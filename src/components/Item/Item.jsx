1c0e9eadf8eb545e3fa780c332763dbaaaad8ff5
zunayedlimon <zahmedlimon@gmail.com>
2023-03-31 15:56:07 +0600

    data hashtag icon added

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,57 +1,65 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Item = (props) => {
-  console.log(props.item);
-  const { author_name, author_image, blog_cover_images, blog_title } =
-    props.item;
+  // console.log(props.item);
+  const { author_name, author_image, blog_cover_images, blog_title } = props.item;
+  
   return (
-
     <div className="flex">
-
-
-      <div className="bg-base-100 w-4/5 mt-10 lg:ms-40 mx-auto">
+      <div className="bg-base-100  mt-10 lg:ms-40 mx-auto">
         <figure>
           <img
             draggable="false"
-            className=" my-4 rounded-lg"
+            style={{ width: "755px" }}
+            className=" my-4 me-0 rounded-lg"
             src={blog_cover_images}
             alt="Shoes"
           />
         </figure>
 
-    <div className="flex justify-between">
-        <div className="flex mb-4" style={{alignItems: "center"}}>
-          <img style={{height: "50px", marginRight: "12px"}} src={author_image} alt="" srcset="" />
+        <div className="flex justify-between" style={{ alignItems: "center" }}>
+          <div className="flex " style={{ alignItems: "center" }}>
+            <img
+              style={{ height: "50px", marginRight: "12px" }}
+              src={author_image}
+              alt=""
+              srcset=""
+            />
+            <div>
+              <p style={{ fontSize: "16px" }} className="font-bold">
+                {author_name}
+              </p>
+              <p style={{ fontSize: "4px !important" }}>
+                March 14 (4 days ago)
+              </p>
+            </div>
+          </div>
+
           <div>
-            <p style={{fontSize: "16px"}} className="font-bold">{author_name}</p>
-            <p style={{fontSize: "4px !important"}}>March 14 (4 days ago)</p>
+            <p>
+              05 min read
+              <FontAwesomeIcon className="ms-3" icon={faBookmark} />
+            </p>
           </div>
         </div>
 
-        <div>
-          <p>05 min read 
-          <FontAwesomeIcon icon={faBookmark}/>
-          </p>
-        </div>
-    </div>
-
-
+        {/* Title Part */}
         <div className="">
-          <h2 style={{userSelect:"none", fontSize:"20px"}} className="card-title">{blog_title}</h2>
+          <h2
+            style={{ userSelect: "none", fontSize: "20px" }}
+            className="card-title mt-4"
+          >
+            {blog_title}
+          </h2>
+        </div>
+
+        <div className="mt-3">
+          <p>#programming #beginners</p>
+          <p className="mt-3 underline text-blue-600">Mark as read</p>
         </div>
       </div>
-
-
-
-
-      <div className="lg:me-40 border border-neutral-400">       
-        <div className="">Limon is a good boy.</div>
-        <div></div>
-      </div>
-
-
     </div>
   );
 };
